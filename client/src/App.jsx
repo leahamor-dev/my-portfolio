aeb1f57b089952b2c0329fd337ece8fce37c2da3
leahamor <ablaza.leahamor@gmail.com>
2024-09-19 19:19:14 +0800

    Add spinner

---
+++ client/src/App.jsx
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import Main from './components/body';
+import Footer from './components/footer';
+import { useDispatch } from 'react-redux';
+import { fetchData } from './actions';
+import SpinnerComponent from './components/Shared/Spinner';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!isLoaded) {
+      dispatch(fetchData());
+    }
+    setIsLoaded(true);
+  }, []);
+
+  return (
+    <>
+      {isLoaded ? (
+        <div className="App">
+          <div className="h-100">
+            <Main />
+            <Footer />
+          </div>
+        </div>
+      ) : (
+        <SpinnerComponent />
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Shared/Spinner/index.jsx
@@ -0,0 +1,25 @@
+import { Spinner } from 'reactstrap';
+import styles from './styles.module.css';
+
+const SpinnerComponent = () => {
+  const loopSpinner = () => {
+    let spinnerArray = [];
+    for (let i = 1; i <= 4; i++) {
+      spinnerArray.push(
+        <Spinner
+          className={`${styles.spinner} mx-2 ${
+            i % 2 == 0 ? styles.spinner_even : styles.spinner_odd
+          }`}
+          type={i % 2 == 0 ? 'border' : 'grow'}
+        >
+          Loading...
+        </Spinner>
+      );
+    }
+    return spinnerArray;
+  };
+
+  return <div className={styles.container}>{loopSpinner()}</div>;
+};
+
+export default SpinnerComponent;
